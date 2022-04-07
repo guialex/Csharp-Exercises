acc5aaea0d76241384c57ed0365f4cd71ac54351
G1Junior <Juninho240830@gmail.com>
2022-04-07 20:41:45 -0300

    Primeiro Commit

---
+++ CopaMundo/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            verificaOpcao(pegarOpcao());
+        }
+
+        private static int pegarOpcao()
+        {
+
+            Console.WriteLine("Fala ai meu consagrado \n" + "Quem ganhou a Copa de 2014?");
+            Console.WriteLine("1. ARGÉLIA \n" + "2. PERU \n" + "3. ALEMANHA \n" + "4. BRASIL \n");
+
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        private static void verificaOpcao(int opcao)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Errou! HAHAHAHA");
+                    break;
+                case 2:
+                    Console.WriteLine("Errou! HAHAHAHA");
+                    break;
+                case 3:
+                    Console.WriteLine("Acertou mizeravi! Parabéns!");
+                    break;
+                case 4:
+                    Console.WriteLine("Errou! HAHAHAHA");
+                    break;
+                default:
+                    Console.WriteLine("Esqueceu do 7x1 é?");
+                    break;
+            }
+        }
+    }
+}
---
+++ IMC/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            classificaPeso(IMC(pegarAltura(), pegarPeso()));
+        }
+
+        private static void classificaPeso(double IMC)
+        {
+            switch (IMC)
+            {
+                case double aux when aux < 18.5:
+                    Console.WriteLine("Você está abaixo do peso");
+                    break;
+                case double aux when aux >= 18.5 && aux <= 24.9:
+                    Console.WriteLine("Você está com peso normal");
+                    break;
+                case double aux when aux >= 25 && aux <= 29.9:
+                    Console.WriteLine("Você está com sobrepeso");
+                    break;
+                case double aux when aux >= 30 && aux <= 34.9:
+                    Console.WriteLine("Você está com obesidade grau I");
+                    break;
+                case double aux when aux >= 35 && aux <= 39.9:
+                    Console.WriteLine("Você está com obesidade grau II");
+                    break;
+                case double aux when aux >= 40:
+                    Console.WriteLine("Você está com obesidade grau III");
+                    break;
+                default:
+                    Console.WriteLine("Classifcação não encontrada");
+                    break;
+            }
+            //if (IMC < 18.5)
+            //{
+            //    Console.WriteLine("Você está abaixo do peso");
+
+            //}
+            //else if (IMC >= 18.5 && IMC <= 24.9)
+            //{
+            //    Console.WriteLine("Você está com peso normal");
+
+            //}
+            //else if (IMC >= 25 && IMC <= 29.9)
+            //{
+            //    Console.WriteLine("Você está com sobrepeso");
+
+            //}
+            //else if (IMC >= 30 && IMC <= 34.9)
+            //{
+            //    Console.WriteLine("Você está com obesidade grau I");
+
+            //}
+            //else if (IMC >= 35 && IMC <= 39.9)
+            //{
+            //    Console.WriteLine("Você está com obesidade grau II");
+            //}
+            //else if (IMC >= 40)
+            //{
+            //    Console.WriteLine("Você está com obesidade grau III");
+            //}
+        }
+
+        public static double pegarPeso()
+        {
+            Console.WriteLine("Opa, me fala ai teu peso");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static double pegarAltura()
+        {
+            Console.WriteLine("Bicho, qual tua altura?");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static double IMC(double altura, double peso)
+        {
+            double IMC = peso / (altura * altura);
+            Console.WriteLine("Cara, teu IMC é: " + IMC.ToString());
+            return IMC;
+        }
+    }
+}
