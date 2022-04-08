e4140b4e314ef906ec26256db4c87aeca771e877
G1Junior <Juninho240830@gmail.com>
2022-04-07 21:11:03 -0300

    primeiro commit

---
+++ DecrescenteApp/DecrescenteApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DecrescenteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decresceNum(pegaNum());
+        }
+
+        private static int pegaNum()
+        {
+            Console.WriteLine("Opa fala um numero ai");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        private static void decresceNum(int num)
+        {
+            for(;num >= 0; num--)
+            {
+                Console.WriteLine(num.ToString());
+            }
+        }
+    }
+}
