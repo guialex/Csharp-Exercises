96ba4fe033bca1fe737e2b18b6b07d0e6bbb6067
guialex <47760385+guialex@users.noreply.github.com>
2022-04-12 22:20:40 -0300

    Add files via upload

---
+++ ArrayToList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayToList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array1 = new int[] { 1,2,3,4,5,6,7,8,9,10};
+            //Console.WriteLine(array1.Length);
+            var lista1 = new List<int> { };
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                lista1.Add(array1[i] * 10);
+
+                Console.WriteLine(array1[i].ToString() + "," + lista1[i].ToString());
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ NomeSobrenome/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NomeSobrenome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nome = new string[,] {
+            {"",""},
+            {"",""},
+            {"",""},
+            {"",""},
+            {"",""},
+            };
+            //inserirNomes();
+
+            for (int linha = 0; linha < nome.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < nome.GetLength(1); coluna++)
+                {
+                    Console.WriteLine("Digite um nome ou um sobrenome");
+                    nome[linha, coluna] = Console.ReadLine();
+                    //Console.WriteLine(nome[linha, coluna]);
+                }
+            }
+
+            Console.WriteLine("-----------------------");
+
+            for (int linha = 0; linha < nome.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < nome.GetLength(1); coluna++)
+                {
+                    Console.Write(nome[linha, coluna] + " ");
+                }
+                Console.WriteLine(" ");
+            }
+
+        }
+
+       // private static void inserirNomes()
+      //  {
+//throw new NotImplementedException();
+       // }
+    }
+}
