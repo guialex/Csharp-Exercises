f7f98ec78381863aeb543a4939888d69337bad49
guialex <47760385+guialex@users.noreply.github.com>
2022-04-12 21:25:54 -0300

    Add files via upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 1;
+            int result = 0;
+
+            while(result <= 144)
+            {
+                result = num1 + num2;
+                Console.WriteLine(num1.ToString() + "+" + num2.ToString() + " = " + result.ToString());
+                num2 = num1;
+                num1 = result;
+
+            }
+        }
+    }
+}
